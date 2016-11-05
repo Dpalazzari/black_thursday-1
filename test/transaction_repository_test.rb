bf905db65859543dc35734be5c7d3f3eada2255f
Drew Palazzari <drewpalazzari@hotmail.com>
2016-11-05 10:36:00 -0600

    transactions and customer repo

--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -1,1 +1,25 @@
+require_relative '../test/test_helper'
+require_relative '../lib/transaction_repository'
+require_relative '../lib/sales_engine'
 
+
+class TransactionRepositoryTest < Minitest::Test
+
+  attr_reader :transaction_repository
+
+  def setup
+    se = SalesEngine.from_csv({
+              :items     => 'data/item_fixture.csv',
+              :merchants => 'data/merchants_fixture.csv',
+              :invoices  => 'data/invoices_fixtures.csv',
+              :invoice_items => './fixture/invoice_item_fixture.csv',
+              :transactions => './fixture/transaction_fixtrue.csv',
+              :customers => './data/customers.csv'})
+    @transaction_repository = se.transactions
+  end
+
+  def test_it_exists
+    assert transaction_repository
+  end
+
+end
--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -8,13 +8,13 @@ class TransactionTest < Minitest::Test
 
   def setup
     @transaction = Transaction.new({
-      :id         => "6",
-      :invoice_id    => "8",
+      :id                 => "6",
+      :invoice_id         => "8",
       :credit_card_number => "4242424242424242",
       :credit_card_expiration_date   => "0220",
-      :result => "success",
-      :created_at => "2012-03-27 14:54:09 UTC",
-      :updated_at => "2012-03-27 14:54:09 UTC"
+      :result             => "success",
+      :created_at         => "2012-03-27 14:54:09 UTC",
+      :updated_at         => "2012-03-27 14:54:09 UTC"
       })
   end
 
